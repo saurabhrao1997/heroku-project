d733820cb0bfab4ba121ddbc9a476fce5107645b
saurabh <saurabhrao1997@gmail.com>
2022-04-11 20:21:23 +0530

    add

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const data =require("./data");
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-   res.send(data)
-})
+// app.get("/",(req,res)=>{
+//    res.send(data)
+// })
 
 app.listen(port,()=>{
     console.log(`listen working on port no ${port}`);
