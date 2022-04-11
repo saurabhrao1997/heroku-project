24da0244f5ac86705a6bf1a8ba28adbd5421d178
saurabh <saurabhrao1997@gmail.com>
2022-04-11 20:10:20 +0530

    new work

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000
+const path = require("path");
+
+const port = process.env.PORT || 3000;
 
 
+app.use(express.static(path.join(__dirname,"./public")))
 
 const data =require("./data");
 
