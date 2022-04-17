89e36d84c14c2a1a45167522ac22541f141a36c8
saurabh <saurabhrao1997@gmail.com>
2022-04-17 15:33:16 +0530

    add data

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const path = require("path");
 const port = process.env.PORT || 3000;
 
 
-app.use(express.static(path.join(__dirname,"./public")))
+//app.use(express.static(path.join(__dirname,"./public")))
 
 const data =require("./data");
 
 
 app.use(express.json());
 
-// app.get("/",(req,res)=>{
-//    res.send(data)
-// })
+app.get("/",(req,res)=>{
+   res.send(data)
+})
 
 app.listen(port,()=>{
     console.log(`listen working on port no ${port}`);
